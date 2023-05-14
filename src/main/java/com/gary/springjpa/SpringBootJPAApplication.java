a9b8e242cd57f09d6e5c35b27f4a9ba8587cd42d
Gary Ma <gary.ma@entelect.co.za>
2023-05-14 15:33:49 +0200

    init commit

---
+++ src/main/java/com/gary/springjpa/SpringBootJPAApplication.java
@@ -0,0 +1,13 @@
+package com.gary.springjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJPAApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJPAApplication.class, args);
+    }
+
+}
